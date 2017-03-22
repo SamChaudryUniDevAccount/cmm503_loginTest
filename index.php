8ac33358ed1985da356ddfebcc32724c2c191225
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-22 23:18:41 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+include("dbconfig.php");
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+    $sql = "SELECT id FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
+    $count = mysqli_num_rows($result);
+
+    if($count == 1) {
+
+        $_SESSION['login_user'] = $myusername;
+
+        header("location: landing.php");
+
+    }else {
+
+        echo  "Your Login Name or Password is invalid";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>It's Alive!!!</h1>
+
+<form action="" method="post">
+    <div class="imgcontainer">
+    </div>
+
+    <div class="container">
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+
+        <button type="submit">Login</button>
+    </div>
+</form>
+
+</body>
+</html>
---
+++ landing.php
@@ -0,0 +1,15 @@
+<?php
+include('session.php');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Im in baby</title>
+</head>
+<body>
+
+</body>
+</html>
